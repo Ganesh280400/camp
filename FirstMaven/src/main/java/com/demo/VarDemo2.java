a61df63f3351b3e3bbf075b00896c798032a932a
Ganesh280400 <ganeshm@90heal.com>
2024-06-12 12:21:47 +0530

    updated how are you

--- FirstMaven/src/main/java/com/demo/VarDemo2.java
+++ FirstMaven/src/main/java/com/demo/VarDemo2.java
@@ -4,7 +4,7 @@ public class VarDemo2 {
 	
 
 	public static void main(String[] args) {
-       System.out.println("Hello World ,Hi Ganesh");
+       System.out.println("Hello World ,How are you?");
 	}
 
 }
