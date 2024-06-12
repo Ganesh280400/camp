410bf4072a1780bb0ec82c275b90fee2973f314a
Ganesh280400 <ganeshm@90heal.com>
2024-06-12 15:11:16 +0530

    variables

---
+++ SecondMaven/src/main/java/com/Test/SecondMaven/VarTest.java
@@ -0,0 +1,17 @@
+package com.Test.SecondMaven;
+
+public class VarTest {
+
+	public static void main(String[] args) {
+	String name="ganesh";
+	String role="developer";
+	int age =24;
+
+	System.out.println("his name is :"+name);
+	System.out.println("his role is :"+role);
+	System.out.println("his age is :"+age);
+	
+
+	}
+
+}
